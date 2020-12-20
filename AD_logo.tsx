c46fd17bd4629e079dd293156a2329a946cedd7c
Dea Omerbegovic <dea.omerbegovic@agiledrop.com>
2020-12-20 21:26:26 +0100

    New map for testing purposes.

---
+++ AD_logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="AD_logo" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../../../Downloads/AD_logo.png" width="100" height="100"/>
+</tileset>
